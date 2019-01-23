39986e48b66cf0e3a12756c17d9fbe14c2a640f7
josebrantes <say.just@gmail.com>
2019-01-23 16:05:13 -0300

    initial commit

---
+++ src/api/config.js
@@ -0,0 +1,13 @@
+export default {
+  appname: 'platzimusic',
+  apiKey: '315dfbf5c3daecb1ab4c1c45111df139',
+  secret: 'aff53405f7eb512278d15e4edc0a90eb',
+  registeredTo: 'josebrantes'
+}
+
+
+
+//Application name	platzimusic
+//API key	315dfbf5c3daecb1ab4c1c45111df139
+//Shared secret	aff53405f7eb512278d15e4edc0a90eb
+//Registered to	josebrantes
